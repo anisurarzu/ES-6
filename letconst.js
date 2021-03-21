60bcb199d3ce71840192405de1468e9c15705c3f
anisurarzu <62845237+anisurarzu@users.noreply.github.com>
2021-03-21 22:51:40 +0600

    update

---
+++ letconst.js
@@ -0,0 +1,25 @@
+//const make it fixed
+//const Name='Arzu';
+//Name='raju';
+//console.log(Name);
+
+//let changable && leek proved
+
+//let number='01789879345';
+//number='01972163102';
+//console.log(number);
+
+
+//using var
+//for(var i=0; i<10; i++){
+    
+//}
+//console.log(i);
+
+
+// using let
+//for(let i=0; i<10; i++){
+    
+//}
+//console.log(i);
+
